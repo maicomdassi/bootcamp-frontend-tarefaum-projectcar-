0c8dd225d5fa9163cacd6b2b5be6c40244cda19f
Michael Zafanelli <maicomdassi@gmail.com>
2022-01-25 12:45:01 -0300

    Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from 'components/Navbar';
 function App() {
    return (
       <>
-        <Navbar/>;
+        <Navbar/>
         <h1>teste</h1>
       </>
    );
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,20 +1,34 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 function Navbar() {
   return (
-    <nav className="bg-secondary">
-      <div>
-        <a href="link">
+    <nav className="navbar navbar-expand-md navbar-light bg-secondary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div>
-          <ul>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#projectcar-navbar"
+          aria-controls="projectcar-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="projectcar-navbar">
+          <ul className="navbar-nav offset-md-9 main-menu">
             <li>
-              <a href="link">HOME</a>
+              <a href="link">Home</a>
             </li>
             <li>
-              <a href="link">CATÁLOGO</a>
+              <a href="link">Catálago</a>
             </li>
-           </ul>
+          </ul>
         </div>
       </div>
     </nav>
